cc135f3172847260540f005357dd04c52f45697e
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2022-01-06 11:25:06 +0100

    Lots of tests that doesnt work

--- FreakyFashionServices.APIGateway/Controllers/GatewayController.cs
+++ FreakyFashionServices.APIGateway/Controllers/GatewayController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreakyFashionServices.APIGateway.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.Text.Json;
+using CatalogService = FreakyFashionServices.APIGateway.Models.DTO.Catalogservice;
 
 namespace FreakyFashionServices.APIGateway.Controllers
 {
@@ -7,12 +10,56 @@ namespace FreakyFashionServices.APIGateway.Controllers
     [ApiController]
     public class GatewayController : ControllerBase
     {
+        private readonly IHttpClientFactory httpClientFactory;
 
-        [HttpGet("{products}")]
+        public GatewayController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("products")]
         public async Task<IActionResult> GetProducts()
         {
+            var productDto = await FetchProducts();
 
-            return Ok();
+            return Ok(productDto);
+        }
+
+        private async Task<ProductDto> FetchProducts()
+        {
+            var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"http://localhost:5000/api/Catalog/")
+            {
+                Headers = { { HeaderNames.Accept, "application/json" }, }
+            };
+
+            var httpClient = httpClientFactory.CreateClient();
+
+            using var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            ProductDto productDto = null;
+
+            if (!httpResponseMessage.IsSuccessStatusCode)
+                return productDto;
+
+            using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var catalogServiceProductDto = await JsonSerializer.DeserializeAsync<CatalogService.ProductDto>(contentStream, options);
+
+            productDto = new ProductDto
+            {
+                Name = catalogServiceProductDto.Name,
+                Description = catalogServiceProductDto.Description,
+                ImgUrl = catalogServiceProductDto.ImgUrl,
+                Price = catalogServiceProductDto.Price,
+                ArticleNumber = catalogServiceProductDto.ArticleNumber,
+                UrlSlug = catalogServiceProductDto.UrlSlug
+            };
+
+            return productDto;
         }
     }
 }
